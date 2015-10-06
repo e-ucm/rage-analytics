243f7f416f3fe05a84a274eaae524eb86a26f188
Manuel Freire <manuel.freire@gmail.com>
2015-10-06 12:40:05 +0200

    Updated docker-compose.yml, added single-step launcher

---
+++ launch-all.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+IMAGES=$(grep "image:" docker-compose.yml \
+  | awk '{print $2, " "}' \
+  | xargs)
+
+function launch() {
+  DELAY=$1
+  shift
+  SERVICES=$@
+  echo
+  echo "... launching $SERVICES and waiting $DELAY seconds ..."
+  echo
+  docker-compose up $SERVICES &
+  sleep "${DELAY}s"
+}
+
+echo "      Downloading images"
+echo "-------------------------------"
+for IMAGE in $IMAGES ; do
+  docker pull $IMAGE
+done
+
+echo "       Launching images"
+echo "-------------------------------"
+launch 60 redis mongo elastic kzk
+launch 50 nimbus lrs
+launch 30 a2 supervisor ui
+launch 10 back front lis
+
+
+echo "     All images launched"
+echo "-------------------------------"
+docker-compose ps
