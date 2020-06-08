da1d41fa0c19101055356b468787466bcb97c260
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2020-06-08 20:40:56 +0200

    Adding a systemd unit to manage startup on reboot

--- vagrant/impress.sh
+++ vagrant/impress.sh
@@ -7,4 +7,10 @@ cd /home/vagrant/rage-analytics
 if [[ ! -e "/home/vagrant/rage-analytics/impress-initialized" ]]; then
   ./formalz.sh
   touch /home/vagrant/rage-analytics/impress-initialized
+  cd /home/vagrant/rage-analytics && ./rage-analytics.sh stop
 fi
+
+cp /vagrant/vagrant/systemd/rage-analytics.service /etc/systemd/system/rage-analytics.service
+chmod 644 /etc/systemd/system/rage-analytics.service
+systemctl enable rage-analytics
+systemctl start rage-analytics
